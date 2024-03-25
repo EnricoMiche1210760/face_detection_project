57aa5b80d681fc9a7132a748e946151c1c34a24a
Enrico Michelon <enrico.michelon@fracarro.com>
2024-03-25 18:27:49 +0100

    wip

--- src/process.py
+++ src/process.py
@@ -4,24 +4,37 @@ import cv2
 from skimage.restoration import denoise_nl_means, estimate_sigma
 from skimage import img_as_float
 import numpy as np
+import user_warnings as uw
 
 zip_file = "img_align_celeba.zip"
 DATA_PATH = "../data"
 
-def extract_dataset(path: str = None):
+def extract_dataset(path: str = None, folder:str = "single_folder"):
     if not path:
         path = DATA_PATH
     if not os.path.exists(path):
-        print("Extracting CelebA dataset...")
+        print("Extracting dataset...")
         with zipfile.ZipFile(DATA_PATH+'/'+zip_file, 'r') as zip_ref:
             zip_ref.extractall(DATA_PATH)
     else:
-        print("CelebA dataset already extracted")
+        if folder == "single_folder":
+            print("Dataset already extracted")
+            return None
+        elif folder == "multi_folders" or folder == "multiple":
+            folder_list = os.listdir(path)
+            return folder_list
+        else:
+            raise ValueError("Invalid folder type")
+
 
 def load_images(path:str, number_of_images=100, random_seed=42):
     if not os.path.exists(path):
         extract_dataset()
     list_of_images = os.listdir(path)
+    if number_of_images > len(list_of_images):
+        uw.fxn()
+        return list_of_images
+    
     np.random.seed(random_seed)
     images = np.random.choice(list_of_images, number_of_images)
     return images
@@ -31,7 +44,7 @@ def load_image(path:str, filename=None):
         if not os.path.exists(path):
             extract_dataset()
         list_of_images = os.listdir(path)
-        filename = DATA_PATH+"/img_align_celeba/"+list_of_images[0]
+        filename = path+list_of_images[0]
         print("Loaded image: ", filename)
     else:
         if filename not in os.listdir(path):
@@ -74,10 +87,10 @@ def extract_features_image(image : np.ndarray, debug=False):
                 break
         cv2.destroyAllWindows()
 
+
+    kp = sorted(kp, key = lambda x:x.response, reverse=True)[:128]
     kp, des = sift.compute(image, kp)
     return des
-
-    
 
 
 
---
+++ src/user_warnings.py
@@ -0,0 +1,4 @@
+import warnings
+
+def fxn():
+    warnings.warn("The requested number of images is greater than the number of images in the folder", UserWarning)
