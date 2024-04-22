f2186596c69af83bed7e1dd47c0719303aa70187
Enrico Michelon <enrico.michelon@fracarro.com>
2024-04-22 17:55:05 +0200

    bug fix and functions comment

--- src/detection.py
+++ src/detection.py
@@ -18,7 +18,7 @@ def sliding_window(image : np.array, window_size : tuple = (32,32), step_size : 
             Step size for moving the window (horizontal_step, vertical_step).
 
     Yields:
-        Tuple containing the sliding window and its coordinates (x, y, roi).
+        Tuple containing the sliding window, its coordinates and the scale factor for the pyramid level it was extracted from (x, y, roi, scale).
     """
 
     scales = [2, 1.5, 1.25, 1, 0.75, 0.5]
@@ -37,9 +37,35 @@ def sliding_window(image : np.array, window_size : tuple = (32,32), step_size : 
 
 
 def sigmoid(x):
+    '''
+    Compute the sigmoid function.
+
+    Parameters:
+        x: numpy.ndarray
+            Input values.
+    Returns:
+        numpy.ndarray
+    '''
     return 1 / (1 + np.exp(-x))
 
-def extract_features(image, pipeline : object, n_keypoints, method='SIFT', equalize=False):    
+def extract_features(image, pipeline : object, n_keypoints, method='SIFT', equalize=False):
+    '''
+    Extract features from an image.
+
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        pipeline: object
+            Pipeline object.
+        n_keypoints: int
+            Number of keypoints to extract.
+        method: str
+            Method to use for feature extraction, either 'SIFT', 'HOG' or 'ORB'.
+        equalize: bool
+            Whether to equalize the image.
+    Returns:
+        numpy.ndarray
+    '''
     
     if method == 'SIFT':
         _, features = pipeline.named_steps['extract_features'](image, n_optimal_keypoints=n_keypoints)
@@ -56,7 +82,25 @@ def extract_features(image, pipeline : object, n_keypoints, method='SIFT', equal
     
     return features
 
-def get_image_prediction(features, pipeline : object, method='SIFT', verbose = False, threshold=0.5):    
+def get_image_prediction(features, pipeline : object, method='SIFT', verbose = False, threshold=0.5):
+    '''
+    Get the prediction for an image.
+
+    Parameters:
+        features: numpy.ndarray
+            Features extracted from the image.
+        pipeline: object
+            Pipeline object.
+        method: str
+            Method used for feature extraction ('SIFT', 'HOG' or 'ORB').
+        verbose: bool
+            Whether to print the prediction.
+        threshold: float
+            Threshold for the prediction.
+    Returns:
+        Tuple containing the prediction and the score.
+    '''
+
     if(method != 'HOG'):
         features = features.flatten()
     if(features.ndim == 1):
@@ -81,30 +125,58 @@ def get_image_prediction(features, pipeline : object, method='SIFT', verbose = F
     return (y_pred, score)
 
 def get_box_around_face(xs, ys, y_pred, scales, wins):
+    '''
+    Get the bounding box around the face.
+    
+    Parameters:
+        xs: list
+            List of x coordinates.
+        ys: list
+            List of y coordinates.
+        y_pred: numpy.ndarray
+            Predictions for each window.
+        scales: list
+            List of scales.
+        wins: list
+            List of windows.
+    Returns:
+        Tuple containing the bounding boxes and the keypoints.
+    '''
+
     face_keypoints = []
     boxes = None
     for i, pred in enumerate(y_pred):
         if pred == 1:
-            print(scales[i])
             scale = scales[i]
-            #if scale > 1:
-            scale = 1/scale
 
-            x, y = int(xs[i]*scale), int(ys[i]*scale)
+            x, y = int(xs[i]//scale), int(ys[i]//scale)
             roi = wins[i]
             face_keypoints.append([roi, xs[i], ys[i]])
 
             if boxes is None:
-                boxes = np.array([x, y, x + roi.shape[1]*scale, y + roi.shape[0]*scale])
+                boxes = np.array([x, y, x + roi.shape[1]//scale, y + roi.shape[0]//scale])
             else:
-                boxes = np.vstack((boxes, [x, y, x + roi.shape[1]*scale, y + roi.shape[0]*scale]))
+                boxes = np.vstack((boxes, [x, y, x + roi.shape[1]//scale, y + roi.shape[0]//scale]))
 
     return boxes, face_keypoints
 
 
-def non_max_suppression(boxes, scores, overlap_threshold):
+def non_max_suppression(boxes : np.ndarray, scores, overlap_threshold=.6):
+    '''
+    Apply non-maximum suppression to the bounding boxes.
+
+    Parameters:
+        boxes: numpy.ndarray
+            Bounding boxes.
+        scores: numpy.ndarray
+            Scores for each bounding box.
+        overlap_threshold: float
+            Threshold for the overlap.
+    Returns:
+        Tuple containing the bounding boxes and the scores.
+    '''
     if boxes is None:
-        return None
+        return (None, None)
     
     print(boxes.shape)
 
@@ -150,6 +222,38 @@ def non_max_suppression(boxes, scores, overlap_threshold):
 
 def detect_faces(image, pipeline : object, method='SIFT', threshold=.5, window_size=(96, 96), \
                  step_size=(16, 16), n_keypoints=500, resize=False, image_size=(96,96), overlap_threshold=.6, verbose=False, notebook=False):
+    '''
+    Detect faces in an image.
+
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        pipeline: object
+            Pipeline object.
+        method: str
+            Method to use for feature extraction ('SIFT', 'HOG' or 'ORB').
+        threshold: float
+            Threshold for the prediction.
+        window_size: tuple
+            Size of the sliding window (width, height).
+        step_size: tuple
+            Step size for moving the window (horizontal_step, vertical_step).
+        n_keypoints: int
+            Number of keypoints to extract.
+        resize: bool
+            Whether to resize the image.
+        image_size: tuple
+            Size of the image after resizing.
+        overlap_threshold: float
+            Threshold for the overlap.
+        verbose: bool
+            Whether to print the prediction.
+        notebook: bool
+            Whether to display the image in a notebook.
+    Returns:
+        Tuple containing the bounding boxes, keypoints and scores.
+    '''
+
     boxes = []
     scores = 0
 
@@ -159,7 +263,7 @@ def detect_faces(image, pipeline : object, method='SIFT', threshold=.5, window_s
     if method != 'HOG':
         image = pipeline.named_steps['preprocess'](image, denoise=True, resize=resize, img_resize=image_size)
     else:
-        image = pipeline.named_steps['preprocess'](image, denoise=False, resize=resize, img_resize=image_size)#, equalize=False)
+        image = pipeline.named_steps['preprocess'](image, denoise=False, resize=resize, img_resize=image_size)
     features = None
 
     xs, ys, wins, scales = zip(*sliding_window(image, window_size=window_size, step_size=step_size))
