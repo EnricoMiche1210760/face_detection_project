2ef2008a3ae44fbf848237b1f83882340cda53fb
Enrico Michelon <enrico.michelon@fracarro.com>
2024-04-23 12:06:23 +0200

    add function descriptions

--- src/process.py
+++ src/process.py
@@ -7,6 +7,22 @@ import matplotlib.pyplot as plt
 SIFT_FEATURES = 128
 
 def extract_patches(img_list, size:tuple=(96,96), n_patches=5000, random_seed=7):
+    '''
+    Extract patches from a list of images.
+
+    Parameters:
+        img_list: list
+            List of image paths.
+        size: tuple
+            Size of the patches (width, height).
+        n_patches: int
+            Number of patches to extract.
+        random_seed: int
+            Random seed for reproducibility.
+    Returns:
+        patches: numpy.ndarray
+
+    '''
     np.random.seed(random_seed)
     patches = np.empty((n_patches, size[0], size[1], 3), dtype=np.uint8)
     patch_for_image = round(n_patches / len(img_list))-1 
@@ -19,6 +35,8 @@ def extract_patches(img_list, size:tuple=(96,96), n_patches=5000, random_seed=7)
             image = cv2.resize(image, (size[0], size[1]))
             patches[tmp_len, ...] = image
             tmp_len += 1
+        if(tmp_len >= n_patches):
+            return patches
 
     for img in img_list:
         image = cv2.imread(img)
@@ -54,6 +72,17 @@ def extract_patches(img_list, size:tuple=(96,96), n_patches=5000, random_seed=7)
     return patches
 
 def equalize_image(image, normalize=False):
+    '''
+    Equalize an image.
+    
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        normalize: bool
+            Whether to normalize the image.
+    Returns:
+        equalized image: numpy.ndarray
+    '''
     if normalize:
         image = np.uint8(cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX))
     if len(image.shape) == 3:
@@ -63,13 +92,41 @@ def equalize_image(image, normalize=False):
     return image
 
 def denoise_image(image):
+    '''
+    Denoise an image using Gaussian blur.
+
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+    Returns:
+        denoised image: numpy.ndarray
+    '''
     image = img_as_float(image)
     denoised_image = cv2.GaussianBlur(image, (5, 5), 0)
     return denoised_image
 
 def process_image(image, denoise:bool=False, equalize:bool=True, resize:bool=False, img_resize:tuple=(64, 64), 
                   diff_of_gaussian:bool=False):
-    
+    '''
+    Process an image, converting it to grayscale, resizing it, denoising it, equalizing it and applying a difference of Gaussian filter.
+
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        denoise: bool
+            Whether to denoise the image.
+        equalize: bool
+            Whether to equalize the image.
+        resize: bool
+            Whether to resize the image.
+        img_resize: tuple
+            Size of the resized image.
+        diff_of_gaussian: bool
+            Whether to apply a difference of Gaussian filter.
+    Returns:
+        processed image: numpy.ndarray
+    '''
+
     if denoise:
         image = denoise_image(image)
         image = np.uint8(cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX))
@@ -89,6 +146,18 @@ def process_image(image, denoise:bool=False, equalize:bool=True, resize:bool=Fal
     return image
 
 def show_image_with_keypoints(image, keypoints, notebook=False):
+    '''
+    Show an image with keypoints.
+
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        keypoints: list
+            List of keypoints.
+        notebook: bool
+            Whether to display the image in a notebook or in a window.
+    '''
+
     img_with_keypoints = cv2.drawKeypoints(image, keypoints, image)
 
     if notebook:
@@ -106,6 +175,21 @@ def show_image_with_keypoints(image, keypoints, notebook=False):
         cv2.destroyAllWindows()
 
 def extract_SIFT_features(image : np.ndarray, n_optimal_keypoints=128, debug:bool=False):
+    '''
+    Extract SIFT features from an image.
+
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        n_optimal_keypoints: int
+            Number of optimal keypoints to extract.
+        debug: bool
+            Whether to show the image with keypoints.
+    Returns:
+        keypoints: list
+        descriptors: numpy.ndarray
+    '''
+
     sift = cv2.SIFT_create(nfeatures=SIFT_FEATURES)
     kpt, des = sift.detectAndCompute(image, None)
     if debug:
@@ -122,6 +206,20 @@ def extract_SIFT_features(image : np.ndarray, n_optimal_keypoints=128, debug:boo
     return (kpt, des)
 
 def extract_ORB_features(image : np.ndarray, n_keypoints:int=500, debug:bool=False):
+    '''
+    Extract ORB features from an image.
+
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        n_keypoints: int
+            Number of keypoints to extract.
+        debug: bool
+            Whether to show the image with keypoints.
+    Returns:
+        keypoints: list
+        descriptors: numpy.ndarray
+    '''
     orb = ORB(n_keypoints=n_keypoints)
     try:
         orb.detect_and_extract(image)
@@ -135,6 +233,25 @@ def extract_ORB_features(image : np.ndarray, n_keypoints:int=500, debug:bool=Fal
     return (kp, des)
 
 def extract_HOG_features(image, cell_size=(8, 8), block_size=(3, 3), nbins=9, debug=False, equalize=False):
+    '''
+    Extract HOG features from an image.
+    
+    Parameters:
+        image: numpy.ndarray
+            Input image.
+        cell_size: tuple
+            Size of the cells.
+        block_size: tuple
+            Size of the blocks.
+        nbins: int
+            Number of bins.
+        debug: bool
+            Whether to show the HOG image.
+        equalize: bool
+            Whether to equalize the image.
+    Returns:
+        features: numpy.ndarray
+    '''
     if equalize:
         image = equalize_image(image)
     features, hog_img = hog(image, orientations=nbins, pixels_per_cell=cell_size,
